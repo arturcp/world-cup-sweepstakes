1525eeb416334e13865ac5b0f93b5508a2de4d23
Artur <artur.prado@gmail.com>
2018-04-03 09:38:16 -0300

    Change games route to use tournament

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :games, only: :index
+  # resources :games, only: :index, scope: :tournament
+
+  resources :tournament, only: :index, param: :slug do
+    get :games, on: :member
+  end
 
   root to: 'home#index'
 end
