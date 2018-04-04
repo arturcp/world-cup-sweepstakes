8bb7070e083e9026b27e2ff21fc522b7f33883f5
Artur <artur.prado@gmail.com>
2018-04-03 22:05:00 -0300

    Create login page and differentiate admin from regular user

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tournament = Tournament.friendly.find(params[:tournament_name])
   end
