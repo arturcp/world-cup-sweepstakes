aed2f8b889a2e5061ee9d758c6dee63b8e44f2c8
Artur <artur.prado@gmail.com>
2018-04-07 11:46:15 -0300

    Include letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :games, only: :index, scope: :tournament
 
