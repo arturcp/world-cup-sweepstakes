6a91d6edf841390f6f1dceed2eaf8b6de930864c
Artur <artur.prado@gmail.com>
2018-05-18 09:15:26 -0300

    Adjust tie rule to not be computed if user guessed score

--- app/models/rules/tie_rule.rb
+++ app/models/rules/tie_rule.rb
@@ -2,10 +2,10 @@
 
 module Rules
   class TieRule < Base
-    SCORE_POINTS = 3
+    SCORE_POINTS = 1
 
     def calculate
-      if guessed_tie?(guess) && guess.game.tie?
+      if wrong_score? && guessed_tie?(guess) && guess.game.tie?
         SCORE_POINTS
       else
         0
@@ -21,6 +21,10 @@ module Rules
     def guessed_tie?(guess)
       guess.host_id.present? && guess.visitor_id.present? &&
         guess.host_score.present? && guess.host_score == guess.visitor_score
+    end
+
+    def wrong_score?
+      guess.game.score != guess.score
     end
   end
 end
